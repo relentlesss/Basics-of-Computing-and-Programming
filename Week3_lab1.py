5f14799e6b84496e6a824e687472e66cd39529aa
relentlesss <50775594+relentlesss@users.noreply.github.com>
2020-08-16 19:06:12 +0200

    Create Week3_lab1.py

---
+++ Week3_lab1.py
@@ -0,0 +1,15 @@
+print("Please enter the number of coins:")
+
+quarters = int(input("# of quarters: "))
+dimes = int(input("# of dimes: "))
+nickels = int(input("# of nickels: "))
+pennies = int(input("# of pennies: "))
+
+total = round((quarters * 0.25) + (dimes * 0.1) + (nickels * 0.05) + (pennies * 0.01), 2)
+
+whole = int(total)
+
+dec = round(total - whole, 2)*100
+dec_up = int(dec)
+
+print("The total is", str(whole), "dollars and", str(dec_up), "cents")
