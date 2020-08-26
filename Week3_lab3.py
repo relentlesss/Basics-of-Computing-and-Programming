6962cfef0b8790d883f3f250daa6c1e6302e0ef5
relentlesss <50775594+relentlesss@users.noreply.github.com>
2020-08-26 11:48:08 +0200

    Update Week3_lab3.py

--- Week3_lab3.py
+++ Week3_lab3.py
@@ -1,3 +1,6 @@
+#Lab 3 from week 3 of the Basics of Computing and Programming from NYUx
+#Author: relentlesss
+
 weight = input('Please enter weight in kilograms: ')
 height = input('Please enter height in meters: ')
 bmi = float(weight)/float(height)**2
